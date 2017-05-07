44f13c8a7627514ffb223eae843f5ee7b44f436a
essvee <svince04@mail.bbk.ac.uk>
2017-05-07 19:04:04 +0100

    Removes unused methods

--- src/main/java/student/EscapeNode.java
+++ src/main/java/student/EscapeNode.java
@@ -9,29 +9,18 @@ public class EscapeNode {
   private Node node;
   private EscapeNode parent;
   private int cost;
-  private int gold;
-
 
   EscapeNode(Node node, EscapeNode parent) {
     this.node = node;
     setParent(parent);
-    this.gold = node.getTile().getGold();
   }
 
-  int getGold() {
-    return this.gold;
-  }
-
-  void clearGold() {
-    this.gold = 0;
-  }
   void setParent(EscapeNode parent) {
-
     if (parent == null) {
       cost = 0;
     } else {
       this.parent = parent;
-      // Calculate the distance by adding weight of edge with parent node to parent's distance
+      // Calculate the cost by adding weight of edge with parent node to parent's distance
       setCost(parent.getCost() + node.getEdge(parent.getNode()).length());
     }
   }
